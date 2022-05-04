c13d6e6078bf558be600bcf73c158cb2020ea228
Ivan Guzman <hello@ivanguzman.dev>
2022-05-04 11:03:22 -0700

    fix: onclick event for calc

--- js/supcalc.js
+++ js/supcalc.js
@@ -1,3 +1,18 @@
+function MM_findObj(n, d) {
+    //v4.01
+    var p, i, x;
+    if (!d) d = document;
+    if ((p = n.indexOf('?')) > 0 && parent.frames.length) {
+        d = parent.frames[n.substring(p + 1)].document;
+        n = n.substring(0, p);
+    }
+    if (!(x = d[n]) && d.all) x = d.all[n];
+    for (i = 0; !x && i < d.forms.length; i++) x = d.forms[i][n];
+    for (i = 0; !x && d.layers && i < d.layers.length; i++)
+        x = MM_findObj(n, d.layers[i].document);
+    if (!x && d.getElementById) x = d.getElementById(n);
+    return x;
+}
 // form validation
 function validateForm() {
     //v4.0
@@ -49,8 +64,10 @@ function validateForm() {
     document.MM_returnValue = errors == '';
 }
 
+document.getElementsByName('Button').onclick(DoCalc);
+
 // Calculator portion of the code
-function DoCalc(form) {
+function DoCalc() {
     let volume = parseFloat(document.ChemForm.Volume.value);
     let volType =
         document.ChemForm.VolumeType.options[
@@ -78,7 +95,7 @@ function DoCalc(form) {
         document.ChemForm.MagProductList.options[
             document.ChemForm.MagProductList.selectedIndex
         ].value;
-    let xxresult = 0;
+    // let xxresult = 0;
     let resultstring = '';
     let cCawarn = '';
     let cCapheffect = '';
